6ae1c247355fa8281c2a128f629860184f5571fa
Kanamori-O <andres.mardones7237@gmail.com>
2025-04-15 10:20:57 -0400

    Variables e integros

--- h.py
+++ h.py
@@ -1,1 +1,16 @@
-print('hello world')
+#variables
+# nombre='Irthur'
+# edad=68
+
+# nombre=input()
+
+#ejemplo de concadenacion
+# print('hello', nombre, 'y su edad es', edad)
+
+# print('hello', nombre, 'y su edad en 5 años será', edad+5
+
+# print('Ingrese un numeros')
+n1=int(input(('Ingrese un numeros ')))
+n2=int(input(('Ingrese un numeros ')))
+print('El resultado de la suma es', n1+n2)
+
