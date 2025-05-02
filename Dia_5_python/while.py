bf6474c258a14a57db6d9e1d13fff4c8111eaff1
kanamori-o <andres.mardones7237@gmail.com>
2025-05-02 14:15:59 -0400

    Dia 5

---
+++ Dia_5_python/while.py
@@ -0,0 +1,77 @@
+import random
+
+# numAzar=random.randint(1,30)
+# print(numAzar)
+
+# dado=random.randint(1,6)
+# print(dado)
+
+## Adivinar el numero
+# 
+# n1=random.randint(1,50)
+# n2=-1
+# while n2!=n1:
+  #      print(n1)
+    # n2=int(input('Ingrese un numero '))
+    # if n2<n1:
+        # print('El numero a adivinar es mayor')
+    # else:
+        # print('El numero a adivinar es menor')
+# print(f'Felicidades, acerto el numero que era {n1}')6
+
+# # Ruleta rusa
+
+# barril=random.randit(1,6)
+# rul=int(input('Dispare : '))
+# while rul!=barril:
+#     rul=int(input('Dispare'))
+# print('BANG!!!')
+
+# La Florida 20%, La pintana 30%, Puente Alto 25%, San Joaquin 15%
+# Grupo familiar: 1=>2%, 2 a 4 =>3%, 5 o mas=>4%
+# Preguntar al usuario en que comuna vive
+# Preguntar al usuario con cuantas personas vive
+# EL arancel actual es de 200.000 por semestre
+# Basados en las respuestas del usuario y en la informacion dada, calcular su descuento
+'''
+ej:
+Ingrese su comuna: La Florida
+Ingrese su grupo familiar (numero entero usted incluido): 4
+El total del descuento es 23%
+El total a pagar es $154.000
+'''
+# aran=200000
+# flor=20
+# pint=30
+# alto=25
+# san=15
+# grup=0
+# com=int(input('''En que comuna vive?
+#               1.- La Florida
+#               2.- La Pintana
+#               3.- Puente Alto
+#               4.- San Joaquin
+#               '''))
+# if com==1:
+#     desc=flor
+# elif com==2:
+#     desc=pint
+# elif com==3:
+#     desc=alto
+# else:
+#     desc=san
+# pers=int(input('Con cuantas personas vive incluyendolo a usted?'))
+# if pers<=1:
+#     grup+=1
+# elif pers>=2 and pers<=4:
+#     grup+=3
+# else:
+#     grup+=4
+# total=grup+desc
+# print(f'Su descuento actual es {total}%')
+# t1=(aran*total)
+# t2=(t1/100)
+# t3=(aran-t2)
+# print(f'Su total con el descuento aplicado es {t3}')
+
+# Ludo
